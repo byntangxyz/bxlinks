22739b8995fc6c9cfa9a8ec6b8504d458842c6be
Fundz <bintangmurtifandy@gmail.com>
2025-04-13 09:27:42 +0700

    Change links and adding AOS

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,24 @@ import {
   CurrencyCircleDollar,
   Globe,
 } from "phosphor-react";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const links = [
   {
     name: "Instagram",
-    url: "https://instagram.com/yourusername",
+    url: "https://instagram.com/byntangxyz",
     icon: <InstagramLogo size={24} weight="fill" />,
   },
   {
     name: "YouTube",
-    url: "https://youtube.com/yourchannel",
+    url: "https://youtube.com/@byntangxyzz",
     icon: <YoutubeLogo size={24} weight="fill" />,
   },
   {
     name: "Saweria",
-    url: "https://saweria.co/yourusername",
+    url: "https://saweria.co/bintangmurtifandy",
     icon: <CurrencyCircleDollar size={24} weight="fill" />,
   },
   {
@@ -43,22 +45,40 @@ function App() {
     clickSound.current.play();
   };
 
+  useEffect(() => {
+    AOS.init({
+      once: false,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <main className="min-h-screen flex flex-col items-center pt-10 px-4 py-6">
+    <main className="min-h-screen flex flex-col items-center pt-10 px-4 py-6 overflow-x-clip">
       {/* Avatar */}
-      <div className="relative h-44 w-44 md:w-60 md:h-60 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:border-4 before:border-transparent before:animate-[spin_6s_linear_infinite] before:bg-[conic-gradient(from_0deg,_red,_orange,_yellow,_green,_blue,_indigo,_violet,_red)] before:blur-sm before:drop-shadow">
-        <img
-          src="/pp.png"
-          className="absolute inset-0 w-full h-full rounded-full object-cover p-1"
-          alt="Profile"
-        />
+      <div className="pt-6 md:pt-8">
+        <div
+          data-aos="fade-up"
+          className="relative h-44 w-44 md:w-60 md:h-60 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:border-4 before:border-transparent before:animate-[spin_6s_linear_infinite] before:bg-[conic-gradient(from_0deg,_red,_orange,_yellow,_green,_blue,_indigo,_violet,_red)] before:blur-sm before:drop-shadow"
+        >
+          <img
+            src="/pp.png"
+            className="absolute inset-0 w-full h-full rounded-full object-cover p-1"
+            alt="Profile"
+          />
+        </div>
       </div>
 
       {/* Nama & Bio */}
-      <h1 className="mt-2 text-white text-4xl font-semibold font-pop">
+      <h1
+        data-aos="fade-up"
+        className="mt-2 text-white text-4xl font-semibold font-pop"
+      >
         byntangxyz
       </h1>
-      <p className="text-white/80 font-inter text-center mx-2">
+      <p
+        data-aos="fade-up"
+        className="text-white/80 font-inter text-center mx-2"
+      >
         Aku memang manusia biasa yang tak sempurna dan kadang salah
       </p>
 
@@ -70,6 +90,7 @@ function App() {
             className={`w-full flex ${
               index % 2 === 0 ? "justify-end" : "justify-start"
             }`}
+            data-aos={`${index % 2 === 0 ? "fade-right" : "fade-left"}`}
           >
             <a
               href={link.url}
@@ -90,13 +111,16 @@ function App() {
         ))}
       </div>
       <div className="mt-6 w-full flex justify-center">
-        <div className="flex flex-col lg:flex-row w-full max-w-3xl bg-white rounded-2xl shadow-lg overflow-hidden">
+        <div
+          data-aos="fade-up"
+          className="flex flex-col lg:flex-row w-full max-w-3xl bg-white rounded-2xl overflow-hidden shadow-2xl"
+        >
           {/* Kiri: Email */}
-          <div className="bg-neutral-200 p-5 lg:w-1/3 flex flex-col justify-center items-center">
-            <h2 className="text-xl font-semibold text-neutral-700">Contact</h2>
+          <div className="bg-neutral-200 p-5 lg:w-1/3 flex flex-col justify-center items-center hover:bg-gradient-to-l hover:from-blue-900 hover:to-cyan-700 text-neutral-700 hover:text-white cursor-pointer">
+            <h2 className="text-xl font-semibold ">Contact</h2>
             <a
               href="mailto:bintangmurtifandy@gmail.com"
-              className="text-sm text-neutral-600 mt-2 select-text break-words"
+              className="text-sm  mt-2 select-text break-words"
             >
               bintangmurtifandy@gmail.com
             </a>
@@ -119,9 +143,11 @@ function App() {
         </div>
       </div>
 
-      <p className="text-center text-white py-8">
-        © 2024 - 2025 byntangxyz. All rights reserved.
-      </p>
+      <div className="pb-8 overflow-hidden">
+        <p data-aos="fade-up" className="text-center text-white py-8 ">
+          © 2024 - 2025 byntangxyz. All rights reserved.
+        </p>
+      </div>
     </main>
   );
 }
